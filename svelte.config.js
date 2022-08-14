5b701c7713ae483990f43cf6b7b248a4997c9866
jadd0 <102069567+jadd0@users.noreply.github.com>
2022-08-14 09:44:29 +0100

    repush

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,10 @@
-// svelte.config.js
-import adapter from '@sveltejs/adapter-static';
-import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 
-export default {
-  kit: {
-    adapter: adapter({
-      // default options are shown. On some platforms
-      // these options are set automatically — see below
-      pages: 'build',
-      assets: 'build',
-      fallback: null,
-      precompress: false
-    }),
-    prerender: {
-      // This can be false if you're using a fallback (i.e. SPA mode)
-      default: true
-    }
-  }
-}
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter()
+	}
+};
+
+export default config;
