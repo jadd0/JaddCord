7d64b1a3f6214b4ef471534acd1480bfdc5f9765
jadd0 <102069567+jadd0@users.noreply.github.com>
2022-08-24 11:50:54 +0100

    animation working decently well working search bar ability to close, not complete

--- src/routes/app/index.svelte
+++ src/routes/app/index.svelte
@@ -12,9 +12,10 @@
 
 	function findFriend() {
 		// if(friendSearchValue == '') return
-		items = friends
-			.filter((user) => user.username.indexOf(friendSearchValue) !== -1)
-			.map((a) => a.username);
+		items = friends.filter(
+			(user) => user.username.indexOf(friendSearchValue) !== -1
+		);
+		// .map(({key, value}) => ({[key.username]: key.name}), ({username: user.username}));
 		console.log(items);
 	}
 
@@ -50,7 +51,40 @@
 	function change() {}
 
 	function expand() {
+		if (!expanded) {
+			const box = document.getElementById("friendSearch");
+			const container = document.getElementById("friendContainer");
+
+			console.log(box.style.height);
+
+			// if (box.style.height === "94vh") {
+			// 	box.style.height = "27px";
+			// 	expanded = false;
+			// 	return;
+			// }
+
+			box.style.cursor = "normal";
+
+			container.style.width = "40vw";
+			// holder.style.width = "30vw"
+			box.style.width = "35vw";
+			box.style.height = "94vh";
+
+			expanded = true;
+			// document.getElementById("searchHolder").style.display = "block"
+			setTimeout(() => {
+				// console.log(document.getElementById("friendSearch").style.display)
+				document.getElementById("searchHolder").style.display = "block";
+				document.getElementById("nothingDisplay").style.display =
+					"block";
+					document.getElementById("searchImage").style.display = "block"
+
+			}, 75);
+			return;
+		}
 		const box = document.getElementById("friendSearch");
+		const container = document.getElementById("friendContainer");
+
 		console.log(box.style.height);
 
 		// if (box.style.height === "94vh") {
@@ -61,9 +95,25 @@
 
 		box.style.cursor = "normal";
 
-		box.style.height = "94vh";
-		expanded = true;
+		container.style.width = "25vw";
+		// holder.style.width = "30vw"
+		box.style.width = "20vw";
+		box.style.height = "35px";
 
+		expanded = true;
+		// document.getElementById("searchHolder").style.display = "block"
+		setTimeout(() => {
+			// console.log(document.getElementById("friendSearch").style.display)
+			document.getElementById("searchHolder").style.display = "none";
+			document.getElementById("nothingDisplay").style.display = "none";
+			document.getElementById("searchImage").style.display = "none"
+		}, 75);
+		expanded = false
+
+		// document.getElementById("friendSearch").style.overflow = "scroll"
+		// const holder = document.getElementById("searchHolder");
+
+		// holder.style.width = "30vw"
 		// box.style.height === "94vh" ? () => {} : () => {}
 
 		// expanded = true
@@ -77,6 +127,8 @@
 		// box.style.height === "94vh"
 		box.style.height = "27px";
 	}
+
+	function close() {}
 </script>
 
 <!-- {#if response.status == 200}
@@ -100,15 +152,28 @@
 								on:input={findFriend}
 								id="search"
 							/>
+							<div id="divider" />
 							{#each items as item}
 								<div id="searchedFriend">
-									{item}
+									<div id="profilePicture" />
+									<h1 id="searchedFriendName">{item.name}</h1>
+									<h2 id="searchedFriendUsername">
+										@{item.username}
+									</h2>
 								</div>
 							{/each}
 							{#if items.length == 0}
-								<h1>Nothing to display</h1>
+								<h1 id="nothingDisplay" style="display: none">
+									Nothing to display
+								</h1>
 							{/if}
 						</div>
+						<button id="cancelButton" on:click={close}
+							><img
+								src="Close-26.png"
+								style="background: none !important "
+							/></button
+						>
 					{/if}
 				</div>
 
@@ -164,19 +229,59 @@
 		text-decoration: none;
 	}
 
+	#cancelButton {
+		position: absolute;
+		top: calc(2% + 5px);
+		left: 90%;
+	}
+
+	#divider {
+		height: 10px;
+	}
+
+	#profilePicture {
+		width: 45px;
+		height: 45px;
+		border-radius: 200px;
+		background: white;
+		padding: 0;
+		margin: 0;
+		position: absolute;
+		top: 60px + n;
+		left: 30px;
+	}
+
+	#searchedFriendName {
+		margin-top: 5px;
+		font-size: 25px;
+		color: rgb(206, 206, 206);
+	}
+
+	#searchedFriendUsername {
+		font-size: 20px;
+		/* padding-bottom: 200px; */
+	}
+
 	#searchedFriend {
 		text-align: center;
-		line-height: 5vh;
-		width: 100%;
-		height: 5vh;
+		line-height: 30px;
+		margin: 0 auto;
+		/* width: 120%; */
+		height: 65px;
 		/* background: #1b1b1b; */
 		border-bottom: solid 1px rgb(145, 145, 145);
 	}
 
+	/* #searchedFriend:nth-child(1) {
+		margin-top: 10px;
+		background: blue
+	} */
+
 	#searchImage {
+		display: none;
 		position: absolute;
-		left: 2vw;
-		top: 2.5vh;
+		left: 3vw;
+		top: calc(2% + 5px);
 		/* margin: 0 auto; */
 		width: 25px;
 		height: 25px;
@@ -189,12 +294,17 @@
 		/* width: 100px;
 		height: 100px; */
 		/* background: red; */
-		width: 17vw;
+		display: none;
+		width: 80%;
+		height: 35px;
 		margin: 0 auto;
+		margin-left: 5%;
 		margin-top: 2vh;
 		background: #1b1b1b;
 		border-radius: 20px;
-		height: 35px;
+
+		transition: all 1s;
+		/* margin-bottom: 0px; */
 		/* display: flex;  */
 		/* justify-content: center;  */
 	}
@@ -205,10 +315,12 @@
 		/* margin-bottom: 20px; */
 		text-align: left;
 		color: white;
-		width: 12.5vw;
+		width: 23vw;
+		height: 34px;
 		background: none;
-		margin-top: 0.4vh;
-		margin-left: 1.4vw;
+		/* margin-top: 0.4vh; */
+		margin-left: 1.9vw;
+
 		/* float: left; */
 	}
 
@@ -227,7 +339,14 @@
 		/* margin-top: 20px; */
 		border-radius: 30px;
 		background: rgb(41, 41, 41);
-		transition: all 0.5s;
+		transition: all 1s;
+		overflow: scroll;
+		-ms-overflow-style: none; /* IE and Edge */
+		scrollbar-width: none;
+	}
+
+	#friendSearch::-webkit-scrollbar {
+		display: none;
 	}
 
 	#friendSearch:hover {
@@ -235,14 +354,14 @@
 	}
 
 	#friendSearch.expanded {
-		/* transition: max-height 0.5s ease-in-out; */
+		/* transition: max-height 1s ease-in-out; */
 		/* height: 94vh; */
 		/* height: 94vh; */
 		background-color: rgb(65, 65, 65);
 		height: 94vh;
 		/* border-radius: 10px; */
 		/* transform: scaleY(10) */
-		/* transition: max-height 0.5s ease-in-out; */
+		/* transition: max-height 1s ease-in-out; */
 	}
 
 	#friendContainer {
@@ -250,6 +369,7 @@
 		height: 100vh;
 		background-color: #1b1b1b;
 		float: left;
+		transition: all 1s;
 	}
 
 	#friend {
